47938c98b99b9674519ab24b53df69e3e6674b6d
Chris Eller <cteller@bsu.edu>
2019-09-19 15:17:41 -0400

    Implement binarySquare

--- src/Engine.java
+++ src/Engine.java
@@ -16,8 +16,22 @@ public class Engine {
 
     }
 
+    /**
+     * Square a given binary input
+     *
+     * <p>
+     *     This function accepts a single binary value as an argument
+     *     and returns the square of that value in binary.
+     * </p>
+     *
+     * @param input A binary number stored in a String as Java
+     *              does not support bitwise math innately.
+     * @return A string containing the squared integer in binary form
+     */
     String binarySquare(String input){
-
+        int base = Integer.valueOf(input, 2);
+        int square = base*base;
+        return Integer.toBinaryString(square);
     }
 
     String binaryRoot(String input){
