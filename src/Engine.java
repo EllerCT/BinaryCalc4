57de73bb2f86c5ae6d98c045805c722567c5e600
Chris Eller <cteller@bsu.edu>
2019-09-19 15:33:19 -0400

    Implement binaryRoot

--- src/Engine.java
+++ src/Engine.java
@@ -34,7 +34,21 @@ public class Engine {
         return Integer.toBinaryString(square);
     }
 
+    /**
+     * Calculate the square root of a binary integer.
+     *
+     * <p>
+     *      This function accepts a single binary value as an argument
+     *      and returns the square root of that value in binary
+     * </p>
+     *
+     * @param input A binary number stored in a String as
+     *              Java does not support bitwise math innately.
+     * @return A string containing the square root, rounded down, in binary form.
+     */
     String binaryRoot(String input){
-
+        int base = Integer.valueOf(input, 2);
+        int root = (int) Math.floor(Math.sqrt( base));
+        return Integer.toBinaryString(root);
     }
 }
