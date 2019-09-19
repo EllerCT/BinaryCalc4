c772e14c1ad5084553f9e81f842230efb9f2fbc2
Chris Eller <cteller@bsu.edu>
2019-09-19 15:02:32 -0400

    Create Engine skeleton

---
+++ src/Engine.java
@@ -0,0 +1,26 @@
+
+public class Engine {
+    String binaryAdd(String input){
+
+    }
+
+    String binarySubtract(String input){
+
+    }
+
+    String binaryMultiply(String input){
+
+    }
+
+    String binaryDivide(String input){
+
+    }
+
+    String binarySquare(String input){
+
+    }
+
+    String binaryRoot(String input){
+
+    }
+}
