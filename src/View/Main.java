500edb33cdaf4f25b8c7f6a08058ff6dddde5bb0
John <japperales@gmial.com>
2019-09-24 10:24:09 -0400

    Fixed Load Path for Main
    -JP

--- src/View/Main.java
+++ src/View/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(new File("src/View/MainUI.fxml").toURI().toURL());
+        FXMLLoader loader = new FXMLLoader(new File("../BinaryCalc4/src/View/MainUI.fxml").toURI().toURL());
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
